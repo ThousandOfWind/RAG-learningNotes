213444fbeb74bb2aada5dce988e693a3875635d1
ThousandsOfWind <jtlajich@163.com>
2024-04-03 21:55:54 +0800

    query

--- testcase.py
+++ testcase.py
@@ -1,3 +1,14 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    
 question_list = [
     "在巴塞罗那可以体验哪些有趣的事情",
     "阿尔罕宫的有趣历史故事",
